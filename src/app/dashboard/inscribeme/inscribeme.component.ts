dc6d9a7972668272d01a5fac6b69713b85011a81
Anthony Solano Lopez <anthonysn16@icloud.com>
2022-06-07 16:58:13 -0500

    se muestran los datos de alumnos y cursos seleccionados, falta hacer el post

--- src/app/dashboard/inscribeme/inscribeme.component.ts
+++ src/app/dashboard/inscribeme/inscribeme.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { AlumnoModel } from '../alumnos/models/AlumnoModel';
 import { AlumnoService } from '../alumnos/services/alumno.service';
@@ -11,6 +12,8 @@ import { CursosService } from '../cursos/services/cursos.service';
 })
 export class InscribemeComponent implements OnInit {
 
+  form;
+
   id_curso      : any;
   id_alumno      : any;
   alumnos : any;
@@ -19,10 +22,18 @@ export class InscribemeComponent implements OnInit {
   seleccionado: boolean = false;
 
   constructor(
+    private _fb: FormBuilder,
     private _cursosServices: CursosService,
     private _alumnoServices: AlumnoService,
     private _activateRoute: ActivatedRoute,
-  ) { }
+  ) {
+    this.form = this._fb.group({
+      nombre_alumno: new FormControl('', [Validators.required]),
+      apellido_p: new FormControl('', [Validators.required]),
+      apellido_m: new FormControl('', [Validators.required]),
+      nombre_curso: new FormControl('', [Validators.required]),
+    })
+   }
 
   ngOnInit(): void {
     this.getCursos();
@@ -47,9 +58,11 @@ export class InscribemeComponent implements OnInit {
     }
 
     getCursoID(id: any) {
+      this.id_curso = this._activateRoute.snapshot.paramMap.get('id');
       this._cursosServices.getCursoByID(id).subscribe(
         (data) => {
-          console.log(data);
+          console.log(data.curso);
+          this.form.controls['nombre_curso'].setValue(data.curso.nombre_curso);
         })
     }
 
@@ -57,18 +70,16 @@ export class InscribemeComponent implements OnInit {
       this.id_alumno = this._activateRoute.snapshot.paramMap.get('id');
       this._alumnoServices.getAlumnoByID(id).subscribe(
         (data) => {
+          this.form.setValue = data;
           console.log(data.alumno);
+          this.form.controls['nombre_alumno'].setValue(data.alumno.nombre_alumno);
+          this.form.controls['apellido_p'].setValue(data.alumno.apellido_p);
+          this.form.controls['apellido_m'].setValue(data.alumno.apellido_m);
         })
     }
 
-    sleccionAlumno(id: any) {
-      this.id_alumno = id;
-      console.log(id);
-    }
-
-    sleccionCurso(id: any) {
-      this.id_curso = id;
-      console.log(id);
+    agregarAlumnoCursos() {
+      
     }
 
   }
